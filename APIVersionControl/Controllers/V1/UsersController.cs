ead56307fe4c2091be8b01f5268ebe63cfc1cd4b
Soel Attala <soelattala7@gmail.com>
2023-07-27 02:16:43 -0300

    start new project and add controller

---
+++ APIVersionControl/Controllers/V1/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIVersionControl.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private const string ApiTestURL = "";
+    }
+}
---
+++ APIVersionControl/DTO/UserData.cs
@@ -0,0 +1,22 @@
+﻿namespace APIVersionControl.DTO
+{
+    public class User
+    {
+        public string? Id { get; set; }
+        public string? title { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? picture { get; set; }
+
+    }
+
+
+    public class UserResponseData
+    {
+        public User[] data { get; set; }
+        public int total { get; set; }
+        public int pages { get; set; }
+        public int limit { get; set; }
+
+    }
+}
